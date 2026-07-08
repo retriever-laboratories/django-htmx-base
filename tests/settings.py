dfe54a2ccf263b1c2511a19d62f3195144db46c6
Seba Pinto <sebastian.pinto.g@usach.cl>
2026-07-08 16:23:56 -0400

    refactor(tests): clean up comments and remove unused settings

--- tests/settings.py
+++ tests/settings.py
@@ -1,7 +1,5 @@
 SECRET_KEY = "test-secret-key"
 
-DEBUG = True
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -10,14 +8,8 @@ DATABASES = {
 }
 
 INSTALLED_APPS = [
-    "django.contrib.contenttypes",
-    "django.contrib.auth",
     "django_htmx_base",
     "tests",
 ]
 
-ROOT_URLCONF = "django_htmx_base.urls"
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-USE_TZ = True
