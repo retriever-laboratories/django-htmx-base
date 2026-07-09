0a2756625131014f4bd983b5ba3cbb519b607772
Seba Pinto <sebastian.pinto.g@usach.cl>
2026-07-09 16:09:51 -0400

    refactor(tests): remove unused import tests from LibraryHealthTests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,14 +4,6 @@ from tests.models import Article
 
 
 class LibraryHealthTests(TestCase):
-    def test_every_module_of_the_library_imports(self):
-        from django_htmx_base import admin  # noqa: F401
-        from django_htmx_base import models  # noqa: F401
-        from django_htmx_base import routers  # noqa: F401
-        from django_htmx_base import urls  # noqa: F401
-        from django_htmx_base import views  # noqa: F401
-        from django_htmx_base import viewsets  # noqa: F401
-
     def test_the_library_can_be_used(self):
         article = Article.objects.create(title="Hello")
 
