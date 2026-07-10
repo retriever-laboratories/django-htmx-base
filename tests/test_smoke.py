37ea2b2e240028db316c6e58ee423ddf3b3c5279
Seba Pinto <sebastian.pinto.g@usach.cl>
2026-07-10 17:23:23 -0400

    feat: implement TestBaseModel and associated viewset, update tests and templates

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,11 +1,25 @@
+import importlib.util
+
 from django.test import TestCase
 
-from tests.models import Article
+from tests.models import TestBaseModel
+
+MODULES = ["admin", "models", "routers", "urls", "views", "viewsets"]
 
 
-class LibraryHealthTests(TestCase):
-    def test_the_library_can_be_used(self):
-        article = Article.objects.create(title="Hello")
+class HealthTestCase(TestCase):
+    def test_modules_exists(self):
+        for name in MODULES:
+            module = f"django_htmx_base.{name}"
+            module_exists = bool(importlib.util.find_spec(module))
+            self.assertEqual(
+                module_exists,
+                True,
+                f"The module '{module}' does not exist.",
+            )
 
-        self.assertIsNotNone(article.id)
-        self.assertTrue(article.is_active)
+    def test_base_model_inheritance(self):
+        instance = TestBaseModel.objects.create(test_charfield="Hello")
+
+        self.assertIsNotNone(instance.id)
+        self.assertTrue(instance.is_active)
