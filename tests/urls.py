c3b4941e7ab0331028d5c80e5fa3dd066c76b73e
javier pinto <javier.pinto.g@usach.cl>
2026-08-17 12:35:09 -0400

    refactor: modify and simplify test following this case

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,5 @@
 # router
 from django_htmx_base.routers import HtmxRouter
-from tests.viewsets import TestBaseModelUserTrackedViewSet
 
 # viewsets
 from tests.viewsets import TestBaseModelViewSet
@@ -8,7 +7,6 @@ from tests.viewsets import TestCustomFormViewSet
 
 router = HtmxRouter()
 router.register("test-base-models", TestBaseModelViewSet)
-router.register("test-base-models-user-tracked", TestBaseModelUserTrackedViewSet)
 router.register("test-custom-form", TestCustomFormViewSet, basename="testform")
 
 urlpatterns = router.urls
