30d2abebb5abc3c8d919aac351778a123a4545b1
javier pinto <javier.pinto.g@usach.cl>
2026-08-18 15:05:52 -0400

    feat(tests): add some tests using django authentication module

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,6 @@
 # router
 from django_htmx_base.routers import HtmxRouter
+from tests.viewsets import TestBaseModelUserTrackedViewSet
 
 # viewsets
 from tests.viewsets import TestBaseModelViewSet
@@ -7,6 +8,7 @@ from tests.viewsets import TestCustomFormViewSet
 
 router = HtmxRouter()
 router.register("test-base-models", TestBaseModelViewSet)
+router.register("test-base-models-user-tracked", TestBaseModelUserTrackedViewSet)
 router.register("test-custom-form", TestCustomFormViewSet, basename="testform")
 
 urlpatterns = router.urls
